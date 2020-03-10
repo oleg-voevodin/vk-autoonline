f6952fba5adaef09ebbe6af9f4cfe3cfd9d82903
Oleg Voevodin <61212513+oleg-voevodin@users.noreply.github.com>
2020-03-10 15:37:59 +0300

    Create offline.py

---
+++ offline.py
@@ -0,0 +1,15 @@
+import vk_api
+import time
+
+print('===== VK AUTO OFFLINE =====')
+login = input('Enter a login: ')
+password = input('Enter a password: ')
+
+vk_session = vk_api.VkApi(login, password)
+vk_session.auth()
+vk = vk_session.get_api()
+
+while True:
+    vk.account.setOffline()
+    print('Offline!')
+    time.sleep(1)
